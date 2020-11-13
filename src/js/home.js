d08788b755382bd6048dbc0c79120a8b7d72cb76
ershadul1 <ershadul.rayhan@gmail.com>
2020-11-13 20:33:20 +0600

    Add styles in home page

--- src/js/home.js
+++ src/js/home.js
@@ -1,4 +1,11 @@
+const container = document.createElement('div');
+container.classList.add('home-container', 'd-flex', 'flex-column', 'justify-center', 'align-center');
 const heading = document.createElement('h1');
-heading.textContent = "REGEX Cafe";
+heading.textContent = "REGEX CAFE";
+heading.classList.add('font-size-3', 'color-1', 'bold', 'font-1', 'p-1', 'text-center');
+const subHeading = document.createElement('h2');
+subHeading.classList.add('font-size-1-5', 'color-2', 'bold', 'font-1', 'text-center');
+subHeading.textContent = "warning! our food may stop your regular expression :)"
+container.append(heading, subHeading);
 
-export default heading; 
+export default container; 
