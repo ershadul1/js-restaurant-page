faf201a8fe4bd5e751109a857a50c26ada66659a
ershadul1 <ershadul.rayhan@gmail.com>
2020-11-13 20:32:57 +0600

    Add styles to navbar

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,13 +1,13 @@
 import tabSwitcher from './tab-switcher'
 
 const navbar = document.createElement('nav');
-navbar.classList.add('d-flex', 'flex-wrap', 'justify-around');
+navbar.classList.add('d-flex', 'flex-wrap', 'justify-around', 'bg-color-1', 'p-1');
 const navItems = ['Home', 'Menu', 'About'];
 
 navItems.forEach(element => {
   const itemDiv = document.createElement('div');
   itemDiv.textContent = element;
-  itemDiv.classList.add('font-size-2', 'hover');
+  itemDiv.classList.add('font-size-2', 'hover', 'p-half', 'font-1', 'color-3');
   itemDiv.onclick = () => tabSwitcher(element);
   navbar.appendChild(itemDiv);
 })
