8a84a7e9a0b9922b76cc877d4ea897106918e429
ershadul1 <ershadul.rayhan@gmail.com>
2020-11-14 11:38:23 +0600

    Add footer contents

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,5 +1,5 @@
 const footer = document.createElement('div');
-footer.textContent = "Footer";
-footer.classList.add('bg-color-1', 'p-2')
+footer.textContent = "Copyright \u00A9 2020 Regex Cafe . All rights reserved.";
+footer.classList.add('bg-color-1', 'p-2', 'font-1', 'font-size-1', 'text-center')
 
 export default footer; 
