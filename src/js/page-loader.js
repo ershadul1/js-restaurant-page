6f9979ef9f1f9ec6ff6939f27268643175aa2d17
ershadul1 <ershadul.rayhan@gmail.com>
2020-11-14 14:08:33 +0600

    Convert function expressions to arrow functions

--- src/js/page-loader.js
+++ src/js/page-loader.js
@@ -2,13 +2,13 @@ import navbar from './navbar';
 import homepage from './home';
 import footer from './footer';
 
-function pageLoader() {
+const pageLoader = () => {
   const container = document.getElementById('content');
   const innerContainer = document.createElement('div');
   innerContainer.classList.add('inner-container');
 
   innerContainer.appendChild(homepage);
   container.append(navbar, innerContainer, footer);
-}
+};
 
 export default pageLoader;
--- src/js/tab-switcher.js
+++ src/js/tab-switcher.js
@@ -2,12 +2,12 @@ import homePage from './home';
 import menuPage from './menu';
 import aboutPage from './about';
 
-function removeCurrentTab() {
+const removeCurrentTab = () => {
   const nodesToRemove = document.querySelector('.inner-container').childNodes;
   nodesToRemove.forEach(node => node.remove());
-}
+};
 
-function tabSwitcher(pageName) {
+const tabSwitcher = (pageName) => {
   removeCurrentTab();
   const innerContainer = document.querySelector('.inner-container');
 
@@ -24,6 +24,6 @@ function tabSwitcher(pageName) {
     default:
       innerContainer.appendChild(homePage);
   }
-}
+};
 
 export default tabSwitcher;
